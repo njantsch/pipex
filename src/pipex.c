11192e3b2d5c1ab462758923c787369eb1ac4ad7
Nicolas Jantsch <njantsch@1-A-4.42heilbronn.de>
2023-07-10 13:31:50 +0200

    fixed some errors with env

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: njantsch <njantsch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 15:08:25 by njantsch          #+#    #+#             */
-/*   Updated: 2023/07/06 14:40:25 by njantsch         ###   ########.fr       */
+/*   Updated: 2023/07/10 13:23:22 by njantsch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,15 @@ void	parent_process(t_pipex *s, char **av, char **envp, int *fd)
 {
 	int	out_file;
 
-	out_file = open(av[4], O_RDWR | O_CREAT | O_TRUNC, 0644);
+	out_file = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (out_file == -1)
 		ft_error("Error trying to open file in parent process", s);
-	dup2(fd[0], STDIN_FILENO);
-	dup2(out_file, STDOUT_FILENO);
+	if (dup2(fd[0], STDIN_FILENO) < 0)
+		ft_error("first dup2 in parent process", s);
+	if (dup2(out_file, STDOUT_FILENO) < 0)
+		ft_error("second dup2 in parent process", s);
 	close(fd[1]);
+	close(out_file);
 	if (get_path(s, av[3], envp) == 1)
 		ft_error("get_path in parent process", s);
 	execve(s->path_to_file, s->args_parent, envp);
@@ -35,9 +38,12 @@ void	child_process(t_pipex *s, char **av, char **envp, int *fd)
 	in_file = open(av[1], O_RDONLY, 0644);
 	if (in_file == -1)
 		ft_error("Error trying to open file in child process", s);
-	dup2(fd[1], STDOUT_FILENO);
-	dup2(in_file, STDIN_FILENO);
+	if (dup2(fd[1], STDOUT_FILENO) < 0)
+		ft_error("first dup2 in child process", s);
+	if (dup2(in_file, STDIN_FILENO) < 0)
+		ft_error("second dup2 in child process", s);
 	close(fd[0]);
+	close(in_file);
 	if (get_path(s, av[2], envp) == 1)
 		ft_error("get_path in child process", s);
 	execve(s->path_to_file, s->args_child, envp);
@@ -61,12 +67,8 @@ int	main(int ac, char **av, char **envp)
 		if (pid1 == -1)
 			ft_error("Error creating child process", s);
 		if (pid1 == 0)
-		{
 			child_process(s, av, envp, fd);
-			ft_terminate_struct(s);
-		}
 		parent_process(s, av, envp, fd);
-		ft_terminate_struct(s);
 	}
 	else
 		return (ft_printf("Syntax error\n"), 1);
